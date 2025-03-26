ec3ff0c7344f9a9159a65d0cbccfed9bd70d1c19
Regand75 <regandess@gmail.com>
2025-03-26 14:46:38 +0300

    the process of creating a personal account

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -3,17 +3,17 @@ import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {DefaultResponseType} from "../../../types/default-response.type";
-import {OrderType} from "../../../types/order.type";
+import {UserInfoType} from "../../../types/user-info.type";
 
 @Injectable({
   providedIn: 'root'
 })
-export class OrderService {
+export class UserService {
 
   constructor(private http: HttpClient) {
   }
 
-  createOrder(params: OrderType): Observable<OrderType | DefaultResponseType> {
-    return this.http.post<OrderType | DefaultResponseType>(environment.api + 'orders', params, {withCredentials: true});
+  updateUserInfo(params: UserInfoType): Observable<DefaultResponseType> {
+    return this.http.post<DefaultResponseType>(environment.api + 'user', params);
   }
 }
--- frontend/src/app/views/personal/info/info.component.ts
+++ frontend/src/app/views/personal/info/info.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PaymentType} from "../../../../types/payment.type";
+import {DeliveryType} from "../../../../types/delivery.type";
+import {FormBuilder, Validators} from "@angular/forms";
+import {UserService} from "../../../shared/services/user.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {OrderType} from "../../../../types/order.type";
+import {UserInfoType} from "../../../../types/user-info.type";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-info',
@@ -7,9 +16,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  deliveryType: DeliveryType = DeliveryType.delivery;
+  deliveryTypes = DeliveryType;
+  paymentTypes = PaymentType;
+
+  userInfoForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    phone: [''],
+    fatherName: [''],
+    paymentType: [PaymentType.cashToCourier],
+    email: ['', Validators.required],
+    street: [''],
+    house: [''],
+    entrance: [''],
+    apartment: [''],
+  });
+
+  constructor(private fb: FormBuilder,
+              private _snackBar: MatSnackBar,
+              private userService: UserService,) {
+  }
 
   ngOnInit(): void {
   }
 
+  changeDeliveryType(deliveryType: DeliveryType): void {
+    this.deliveryType = deliveryType;
+    // this.userInfoForm.get('deliveryType')?.setValue(deliveryType);
+    this.userInfoForm.markAsDirty();
+  }
+
+  updateUserInfo(): void {
+    if (this.userInfoForm.valid) {
+      const paramsObject: UserInfoType = {
+        deliveryType: this.deliveryType,
+        paymentType: this.userInfoForm.value.paymentType ? this.userInfoForm.value.paymentType : PaymentType.cashToCourier,
+        email: this.userInfoForm.value.email ? this.userInfoForm.value.email : '',
+      };
+      if (this.userInfoForm.value.firstName) {
+        paramsObject.firstName = this.userInfoForm.value.firstName;
+      }
+      if (this.userInfoForm.value.lastName) {
+        paramsObject.lastName = this.userInfoForm.value.lastName;
+      }
+      if (this.userInfoForm.value.fatherName) {
+        paramsObject.fatherName = this.userInfoForm.value.fatherName;
+      }
+      if (this.userInfoForm.value.phone) {
+        paramsObject.phone = this.userInfoForm.value.phone;
+      }
+      if (this.userInfoForm.value.street) {
+        paramsObject.street = this.userInfoForm.value.street;
+      }
+      if (this.userInfoForm.value.house) {
+        paramsObject.house = this.userInfoForm.value.house;
+      }
+      if (this.userInfoForm.value.entrance) {
+        paramsObject.entrance = this.userInfoForm.value.entrance;
+      }
+      if (this.userInfoForm.value.apartment) {
+        paramsObject.apartment = this.userInfoForm.value.apartment;
+      }
+
+      this.userService.updateUserInfo(paramsObject)
+        .subscribe({
+          next: (data: DefaultResponseType) => {
+            if (data.error) {
+              this._snackBar.open(data.massage);
+              throw new Error(data.massage);
+            }
+            this._snackBar.open('Данные успешно сохранены');
+          },
+          error: (errorResponse: HttpErrorResponse) => {
+            if (errorResponse.error && errorResponse.error.message) {
+              this._snackBar.open(errorResponse.error.message);
+            } else {
+              this._snackBar.open('Ошибка сохранения');
+            }
+          }
+        });
+    }
+  }
 }
