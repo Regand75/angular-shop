4acfdf115ec2a4e8339b12af8d8cc846333498cf
regand75 <regandess@gmail.com>
2025-03-31 21:52:19 +0300

    creating functionality for favorite buttons

--- frontend/src/app/views/personal/favorite/favorite.component.ts
+++ frontend/src/app/views/personal/favorite/favorite.component.ts
@@ -3,6 +3,8 @@ import {FavoriteService} from "../../../shared/services/favorite.service";
 import {FavoriteType} from "../../../../types/favorite.type";
 import {DefaultResponseType} from "../../../../types/default-response.type";
 import {environment} from "../../../../environments/environment";
+import {CartService} from "../../../shared/services/cart.service";
+import {CartType} from "../../../../types/cart.type";
 
 @Component({
   selector: 'app-favorite',
@@ -11,20 +13,74 @@ import {environment} from "../../../../environments/environment";
 })
 export class FavoriteComponent implements OnInit {
 
+  cart: CartType | null = null;
+
   products: FavoriteType[] = [];
   serverStaticPath = environment.serverStaticPath;
 
-  constructor(private favoriteService: FavoriteService) {
+  constructor(private favoriteService: FavoriteService, private cartService: CartService) {
   }
 
   ngOnInit(): void {
+
     this.favoriteService.getFavorites()
-      .subscribe((data: FavoriteType[] | DefaultResponseType) => {
-        if ((data as DefaultResponseType).error !== undefined) {
-          const error = (data as DefaultResponseType).massage;
-          throw new Error(error);
+      .subscribe((favoritesData: FavoriteType[] | DefaultResponseType) => {
+        if ((favoritesData as DefaultResponseType).error !== undefined) {
+          throw new Error((favoritesData as DefaultResponseType).massage);
         }
-        this.products = data as FavoriteType[];
+
+        this.cartService.getCart()
+          .subscribe((cartData: CartType | DefaultResponseType) => {
+            if ((cartData as DefaultResponseType).error !== undefined) {
+              throw new Error((cartData as DefaultResponseType).massage);
+            }
+            this.cart = cartData as CartType;
+            this.products = (favoritesData as FavoriteType[]).map(product => {
+              const productInCart = this.cart?.items.find(item => item.product.id === product.id);
+              return {
+                ...product,
+                count: productInCart ? productInCart.quantity : 1,
+                countInCart: productInCart ? productInCart.quantity : 0
+              };
+            });
+          });
+      });
+  }
+
+  addToCart(product: FavoriteType): void {
+    if (product.count) {
+      this.cartService.updateCart(product.id, product.count)
+        .subscribe((data: CartType | DefaultResponseType) => {
+          if ((data as DefaultResponseType).error !== undefined) {
+            throw new Error((data as DefaultResponseType).massage);
+          }
+          product.countInCart = product.count;
+
+        });
+    }
+  }
+
+  updateCount(product: FavoriteType, value: number): void {
+    product.count = value;
+    if (product.countInCart) {
+      this.cartService.updateCart(product.id, product.count)
+        .subscribe((data: CartType | DefaultResponseType) => {
+          if ((data as DefaultResponseType).error !== undefined) {
+            throw new Error((data as DefaultResponseType).massage);
+          }
+          product.countInCart = product.count;
+        });
+    }
+  }
+
+  removeFromCart(product: FavoriteType): void {
+    this.cartService.updateCart(product.id, 0)
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).massage);
+        }
+        product.countInCart = 0;
+        product.count = 1;
       });
   }
 
@@ -36,7 +92,7 @@ export class FavoriteComponent implements OnInit {
           throw new Error(data.massage);
         }
 
-        this.products =  this.products.filter(item => item.id !== id);
+        this.products = this.products.filter(item => item.id !== id);
       })
   }
 }
